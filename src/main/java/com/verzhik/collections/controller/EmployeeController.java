319b05bcd3b9c6e2085d77c9e6f97b0c6e2f6338
verzhik <verzhik11@gmail.com>
2023-04-01 13:46:57 +0300

    ДЗ выполнено

--- src/main/java/com/verzhik/collections/controller/EmployeeController.java
+++ src/main/java/com/verzhik/collections/controller/EmployeeController.java
@@ -1,33 +1,48 @@
 package com.verzhik.collections.controller;
 
+import com.verzhik.collections.objects.Employee;
 import com.verzhik.collections.services.EmployeeServices;
+import com.verzhik.collections.services.EmployeeServicesList;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
-    public EmployeeServices employeeServices;
+    //public EmployeeServices employeeServices;
+    public EmployeeServicesList employeeServicesList;
 
-    public EmployeeController(EmployeeServices employeeServices) {
-        this.employeeServices = employeeServices;
+    //public EmployeeController(EmployeeServices employeeServices) {
+        //this.employeeServices = employeeServices;
+    //}
+
+    public EmployeeController(EmployeeServicesList employeeServicesList) {
+        this.employeeServicesList = employeeServicesList;
     }
 
     @GetMapping
     public String start() {
         return "Hello";
     }
+
     @GetMapping(path = "/add")
-    public String addEmployee(@RequestParam(required = false) String name, @RequestParam(required = false) String lastname) {
-        return name + " " + lastname;
-
-
+    public Employee addEmployee(@RequestParam(required = false) String name, @RequestParam(required = false) String lastname) {
+        return employeeServicesList.addEmployee(name, lastname);
     }
-
-
-
-
-
+    @GetMapping(path = "/remove")
+    public Employee removeEmployee(@RequestParam(required = false) String name, @RequestParam(required = false) String lastname) {
+        return employeeServicesList.removeEmployee(name, lastname);
+    }
+   @GetMapping(path = "/find")
+   public Employee findEmployee(@RequestParam(required = false) String name, @RequestParam(required = false) String lastname) {
+         return employeeServicesList.findEmployee(name, lastname);
+    }
+    @GetMapping(path = "/print")
+   public List<Employee> printEmployee(@RequestParam(required = false) String name, @RequestParam(required = false) String lastname) {
+         return employeeServicesList.printEmployee(name, lastname);
+    }
 }
--- src/main/java/com/verzhik/collections/exceptions/EmployeeStorageIsFullException.java
+++ src/main/java/com/verzhik/collections/exceptions/EmployeeStorageIsFullException.java
@@ -1,5 +1,9 @@
 package com.verzhik.collections.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.PAYLOAD_TOO_LARGE)
 public class EmployeeStorageIsFullException extends RuntimeException {
     public EmployeeStorageIsFullException(String message) {
         super(message);
